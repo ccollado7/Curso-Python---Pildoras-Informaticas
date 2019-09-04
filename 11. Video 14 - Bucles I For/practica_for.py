fca80fa10d0fa6c93dc8f464974c462ec1d89d5f
Collado Claudio <46108725+ccollado7@users.noreply.github.com>
2019-09-04 11:21:49 -0300

    Add files via upload

---
+++ 11. Video 14 - Bucles I For/practica_for.py
@@ -0,0 +1,12 @@
+#Defino un bucle DEFINIDO del tipo for que utiliza como variable a recorrer los valores numericos de una tupla
+
+for i in [1,2,3]:
+	print("hola")
+
+print() #Dejo un espacio en blanco
+
+
+#Defino un bucle DEIFNIDO del tipo for que utiliza como variable a recorrer 4 strings
+
+for i in ["primavera", "verano", "otoño", "invierno"]:
+	print(i)
