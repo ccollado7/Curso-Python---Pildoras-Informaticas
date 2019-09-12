7ba3216eb7311c2e1a97d6a0294e987a90736b24
Collado Claudio <46108725+ccollado7@users.noreply.github.com>
2019-09-12 12:32:24 -0300

    Add files via upload

---
+++ 18. Video 21 - Excepciones I/excepciones.py
@@ -0,0 +1,39 @@
+def suma(num1,num2):
+	return(num1 + num2)
+
+def resta(num1,num2):
+	return(num1 - num2)
+
+def multiplica(num1,num2):
+	return(num1 * num2)
+
+def divide (num1,num2):
+	try: #Se indica que se "intente" realizar esta operacion. En caso que no lo consiga ejecutara lo que se encuentre dentro del bloque except
+		return(num1/num2)
+
+	except ZeroDivisionError: #Bloque de codigo que se ejecutara en caso que falla el bloque try. Importante la especificacion del tipo de error
+		print("No se puede dividir entre cero")
+		return("Operacion erronea")
+
+op1 = int(input("Introduce el primer numero: "))
+op2 = int(input("Introduce el segundo numero: "))
+
+operacion = str(input("Introduce la operacion a realizar(suma,resta,multiplica,divide: "))
+
+if operacion == "suma":
+	print(suma(op1,op2))
+
+elif operacion == "resta":
+	print(resta(op1,op2))
+
+elif operacion == "multiplica":
+	print(multiplica(op1,op2))
+
+elif operacion == "divide":
+	print(divide(op1,op2))
+
+else:
+	print("Operacion seleccionada no contemplada en el programa")
+
+print("Operacion ejecutada. Continuacion de ejecucion del Programa")
+
